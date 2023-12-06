7e1427367edf43b71eb47015bfed26d223866f17
bruh.ngl0 <adityashrm500@gmail.com>
2023-12-06 17:02:35 +0530

    commi

--- main.js
+++ main.js
@@ -92,3 +92,78 @@ PS D:\leon\codewars> node main.js
 default: 10.406s
 PS D:\leon\codewars> */
 
+
+//counter function
+
+//transactions question
+
+/*const data = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+];
+
+
+
+function easy(tranactions) {
+
+  let total = {}
+
+  tranactions.forEach((block)=>{
+   const {category, price} = block
+    
+
+    if (!total[category]) {
+      total[category] = price
+    
+    } else {
+      total[category] += price;
+    }
+  
+  });
+
+  const result = Object.keys(total).map(category => ({
+    category,
+    totalSpent: total[category],
+  }));
+
+  console.log(result);
+  }
+
+
+
+
+easy(data) */
+
